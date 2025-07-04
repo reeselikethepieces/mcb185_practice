61b4de9ad5bf3c64731566d392a11cbc1b5bc077
reeselikethepieces <charisse.carlson@gmail.com>
2025-07-04 13:57:18 -0700

    up

--- 1PP.py
+++ 1PP.py
@@ -4,18 +4,20 @@
 # write a fxn that det if a number is an int
 
 def is_integer(n):
-	if n % 2 == 0: return True
+	if n % 1 == 0: return True
 	else:          return False
 
 print(is_integer(1))
 print(is_integer(12))
 print(is_integer(-6))
+print(is_integer(1.0))
+print(is_integer(3.132))
 
 # write a fxn that determines if a number is a valid prob
 
 def is_validprob(n):
-	if n >= 0 and n <= 1: return 'is valid'
-	else:                 return 'is not valid'
+	if n >= 0 and n <= 1: return True
+	else:                 return False
 # given multiple probabilities, an additional condition is necessary (sum of n's == 1)
 
 print(is_validprob(1))
@@ -31,7 +33,7 @@ def mw_dna(nt):
 	if nt == 'C': return 289.2
 	if nt == 'G': return 329.2
 	if nt == 'T': return 304.2
-	else:         return 'None'
+	else:         return None
 
 print(mw_dna('C'))
 print(mw_dna('c'))
@@ -44,7 +46,7 @@ def dna_comp(nt):
 	if nt == 'C': return 'G'
 	if nt == 'G': return 'C'
 	if nt == 'T': return 'A'
-	else:         return 'None'
+	else:         return None
 
 print(dna_comp('C'))
 print(dna_comp('A'))
@@ -80,5 +82,30 @@ print(statvals(0, 5, 2, 0))
 
 # write a fxn that returns shannon entropy for nt counts
 
-# def shentropy(A, C, G, T):
+import math
+
+def shentropy(A, C, G, T):	
 	
+	total = A + C + G + T
+	if total == 0: return 0
+	
+	pa = A/total
+	pc = C/total
+	pg = G/total
+	pt = T/total
+	
+	if pa > 0: ea = -pa * math.log2(pa)
+	else:      ea = 0
+	if pc > 0: ec = -pc * math.log2(pc)
+	else:      ec = 0
+	if pg > 0: eg = -pg * math.log2(pg)
+	else:      eg = 0
+	if pt > 0: et = -pt * math.log2(pt)
+	else:      et = 0.0
+	
+	return ea + ec + eg + et
+	
+print(shentropy(0, 0, 0, 0))
+print(shentropy(8, 6, 6, 8))
+print(shentropy(0, 5, 5, 6))
+print(shentropy(0, 5, 2, 0))
