23f68f1ed9fff334cd715ed014ce8c87fe7c737a
reeselikethepieces <charisse.carlson@gmail.com>
2025-07-03 18:09:46 -0700

    up

---
+++ 1AE.py
@@ -0,0 +1,11 @@
+# Assessment Examples
+
+
+# fxn that computs distance b/w 2 points in graph
+
+def euclid_d(x1, y1, x2, y2): return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+print(euclid_d(2, 3, 5, 8))
+
+
+# the rest of the examples were complete and can be seen in 1PP.py
