d7fb1c49e0089cf2923b153d8ee2cd72faebdb67
reeselikethepieces <charisse.carlson@gmail.com>
2025-07-04 15:29:25 -0700

    up

--- 12phred.py
+++ 12phred.py
@@ -5,7 +5,7 @@
 import math
 
 
-def char_to_prob(c): return ord(c) - 33
+def char_to_prob(c): return 10**((ord(c) - 33)/-10)
 
 print(char_to_prob('A'))
 print(char_to_prob('B'))
@@ -13,7 +13,7 @@ print(char_to_prob('!'))
 
 
 def prob_to_char(p): 
-	if p <= 0 or p >= 1: return 'None'
+	if p <= 0 or p >= 1: return None
 	else:                return chr(int(-10 * math.log10(p)) + 33)
 	
 print(prob_to_char(0.001))
