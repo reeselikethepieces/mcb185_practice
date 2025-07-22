fc542194f7ea4f1d4ded111237866d423ffa7a0c
reeselikethepieces <charisse.carlson@gmail.com>
2025-07-22 13:31:44 -0700

    up

--- teachingpython.py
+++ teachingpython.py
@@ -1,4 +1,6 @@
-# adding more while loop examples
+# adding more loop examples
+
+# while loop examples
 	# two types of while loops: `while True` and `while`
 	# the first returns a value, endlessly, until 'break'
 	# the second returns boolean T/F, and will end based on a provided condition
@@ -15,7 +17,12 @@ while True:
 # while False - we don't use. Why? see print statement
 while False:
 	print('this will print')
-# `while` some condition 
+# `while` some condition         ~~~> needs update
+
+# for loop examples
+	# for i in range()
+	# for _ in range() --> oNlY when throwing away '_' value 
+	# for thing in container 
 	
 # side by side 
 limit = 12
@@ -24,3 +31,15 @@ for i in range(limit):
 	
 s = 'ABCDEFGHIJ'
 print(s[0:8:2])
+
+
+
+'''
+misc notes
+
+def init_vals(equation, value):
+-------------------------------
+	if equation == sum: return 'you initialize with 0'
+	if equation == product: return 'you initialize with 1'
+	if value    != sum or product: return: 'initialize with `None` or an empty [container]'
+
