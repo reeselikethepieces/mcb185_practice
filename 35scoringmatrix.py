ad35d33cb25ac89edaa07d8173f1d8bb1975770b
reeselikethepieces <charisse.carlson@gmail.com>
2025-08-03 13:38:49 -0700

    up

--- 35scoringmatrix.py
+++ 35scoringmatrix.py
@@ -4,7 +4,6 @@ alpha = str(sys.argv[1])
 mat = int(sys.argv[2])
 mis = int(sys.argv[3])
 
-rows = []
 for i in range(5):
 	row = []
 	
@@ -15,18 +14,15 @@ for i in range(5):
 		
 		for letter in alpha:
 			row.append(letter)
-			
-		str_row = []
-		for val in row:
-			str_row.append(str(val))
 		
-		print(' '.join(str_row))
-	
-	# row 2
-		if i == 1:
-		# nt 1
-		row.append(alpha[0])
+		print('   '.join(row))
+		
+	else:
+		pairwise = alpha[i-1]
+		row.append(pairwise)
 		
 		for letter in alpha:
-		
+			if letter == pairwise: row.append(f'+{mat}')
+			else:                  row.append(str(mis))
 			
+		print('  '.join(row))
