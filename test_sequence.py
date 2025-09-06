225c7a4f03b0da2be330a9eac24e7157b81b2ac9
reeselikethepieces <charisse.carlson@gmail.com>
2025-09-06 15:56:10 -0700

    up

--- test_sequence.py
+++ test_sequence.py
@@ -3,11 +3,15 @@
 
 import sequence
 
-print(sequence.transcribe('ACGT'))
-print(sequence.revcomp('AAACGT'))
-print(sequence.translate('ATGCCCTAA'))
+# print(sequence.transcribe('ACGT'))
+# print(sequence.revcomp('AAACGT'))
+# print(sequence.translate('ATGCCCTAA'))
 
 # calling for `43skew.py`
-s = 'ACGTGGGGGGCATATG'
-print(sequence.gc_comp(s))
-print(sequence.gc_skew(s), sequence.gc_skew(sequence.revcomp(s)))
+s = 'ACGTGGGGGGCATATGTACGTCCCCC'
+# print(sequence.gc_comp(s))
+# print(sequence.gc_skew(s), sequence.gc_skew(sequence.revcomp(s)))
+
+# calling for `44skew.py`
+print(sequence.gc_comp(s), sequence.gc_skew(s))
+
