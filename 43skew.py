3184ebafd6ce6d7e5d8ab1c7b338e68d79ce451f
reeselikethepieces <charisse.carlson@gmail.com>
2025-09-06 15:56:16 -0700

    up

--- 43skew.py
+++ 43skew.py
@@ -18,4 +18,4 @@ seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
 w = 10
 for i in range(len(seq) -w +1):
 	s = seq[i:i+w]
-	print(i, sequence.gc_comp(s), sequence.gc_skew(s))
+	print(sequence.gc_comp(s), sequence.gc_skew(s))
