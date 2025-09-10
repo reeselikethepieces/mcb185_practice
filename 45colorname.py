78492bf66720a193ac5dd64e6c6d254de9bb4daa
reeselikethepieces <charisse.carlson@gmail.com>
2025-09-09 21:35:53 -0700

    up

--- 45colorname.py
+++ 45colorname.py
@@ -4,11 +4,30 @@
 
 import sys
 colorfile = sys.argv[1]
+'''
 R = int(sys.argv[2])
 G = int(sys.argv[3])
 B = int(sys.argv[4])
+'''
 
-# PICK UP HERE and draw out to confirm/deny conceptual thought below
+# to test code 
+R1 = 200
+G1 = 0
+B1 = 50
+input_color = (R1, G1, B1)
+
+def euclidean(R1, G1, B1, R2, G2, B2):
+	return ((R1 - R2)**2 + (G1 - G2)**2 + (B1 - B2)**2))**0.5
+
+with open(colorfile) as fp:
+	for line in fp:
+		columns = line.split()
+		# PICK UP HERE 
+		print(columns[2]) #string
+		print(columns[2]) #coordinates in containers (lists)
+			
+
+
 '''
 conceptually:
 input 3 values (CL inputs),
@@ -20,5 +39,6 @@ output 1 value (color name)
 '''
 
 '''
-CL input: python3 45colorname.py ~/Code/MCB185/data/colors_extended.tsv 200 0 50
+after soft linking: ln -s ~/Code/MCB185/data/colors_extended.tsv ./colors
+CL input: python3 45colorname.py colors 200 0 50
 '''
